1f8ba20bfec649c6230bec5b407b6a052d656c56
abhi27-web <57997268+abhi27-web@users.noreply.github.com>
2021-02-02 12:31:22 +0530

    Update snake.js

--- snake.js
+++ snake.js
@@ -115,43 +115,6 @@ function init(){
 
 	document.addEventListener('keydown',keyPressed) ;
 	
-	let touchstartX = 0;
-	let touchstartY = 0;
-	let touchendX = 0;
-	let touchendY = 0;
-
-	const gestureZone = document.getElementById('modalContent');
-
-	gestureZone.addEventListener('touchstart', function(event) {
-	    touchstartX = event.changedTouches[0].screenX;
-	    touchstartY = event.changedTouches[0].screenY;
-	}, false);
-
-	gestureZone.addEventListener('touchend', function(event) {
-	    touchendX = event.changedTouches[0].screenX;
-	    touchendY = event.changedTouches[0].screenY;
-	    handleGesture();
-	}, false); 
-
-	function handleGesture() {
-	    if (touchendX < touchstartX) {
-		snake.direction = "left";
-	    }
-
-	    if (touchendX > touchstartX) {
-		snake.direction = "right";
-	    }
-
-	    if (touchendY < touchstartY) {
-		snake.direction = "up";
-	    }
-
-	    if (touchendY > touchstartY) {
-	       snake.direction = "down";
-	    }
-	}
-
-	
 }
 
 
